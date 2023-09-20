36e83023702cc478eb8aec12c58801867aae629e
Murat Ay <murataybtp@gmail.com>
2023-09-20 23:00:04 +0300

    Suppress, disabled message

--- XInfrastructure/XDateTimeConverter.cs
+++ XInfrastructure/XDateTimeConverter.cs
@@ -5,23 +5,20 @@ namespace XInfrastructure;
 
 public class XDateTimeConverter : JsonConverter<DateTime>
 {
-    private const string DateTimeFormat = "yyyyMMdd HHmmss fff";
-
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.String)
         {
-            if (DateTime.TryParseExact(reader.GetString(), DateTimeFormat, null, System.Globalization.DateTimeStyles.None, out DateTime parsedDateTime))
+            if (DateTime.TryParseExact(reader.GetString(), XConstant.DateTimeFormat, null, System.Globalization.DateTimeStyles.None, out DateTime parsedDateTime))
             {
                 return parsedDateTime;
             }
         }
-
-        throw new JsonException($"Unable to parse DateTime in the expected format ({DateTimeFormat}).");
+        throw new JsonException($"Unable to parse DateTime in the expected format ({XConstant.DateTimeFormat}).");
     }
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
     {
-        writer.WriteStringValue(value.ToString(DateTimeFormat));
+        writer.WriteStringValue(value.ToString(XConstant.DateTimeFormat));
     }
 }
