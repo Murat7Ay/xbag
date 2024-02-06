f75ff7ecdb1d563e99a4d764661d80837cfa43e4
Murat Ay <murataybtp@gmail.com>
2024-02-06 23:37:53 +0300

    bloom filter operations added

--- X.Api/RegisterInfrastructures.cs
+++ X.Api/RegisterInfrastructures.cs
@@ -1,33 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Redis.OM;
-using StackExchange.Redis;
 using XDataAccess;
+using XProbabilisticToolkit;
 
-public static class RegisterEntities
+namespace X.Api;
+
+public static class RegisterInfrastructures
 {
+    public static void AddProbabilistic(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton(typeof(IBloomFilter), typeof(BloomFilter));
+    }
+
     public static void AddRepositories(WebApplicationBuilder builder)
     {
         builder.Services.AddSingleton(typeof(IRepository<>), typeof(RedisRepository<>));
     }
+
     public static void AddApis(WebApplication app)
     {
-        // app.MapPut("/rose", (IRepository<RoseEntity> repository, RoseEntity entity) => repository.Save(entity)).RequireAuthorization("root");
-        // app.MapPost("/rose", (IRepository<RoseEntity> repository, RoseEntity entity) => repository.Update(entity)).RequireAuthorization("root");
-        // app.MapDelete("/rose", (IRepository<RoseEntity> repository, string id) => repository.Delete(id)).RequireAuthorization("root");
-        // app.MapGet("/rose", (IRepository<RoseEntity> repository) => repository.Get()).RequireAuthorization("root");
-        // app.MapGet("/rose/{id}/history", (IRepository<RoseEntity> repository, string id) => repository.GetHistory(id)).RequireAuthorization("root");
-        // app.MapGet("/rose/{id}", (IRepository<RoseEntity> repository, string id) => repository.FindById(id)).RequireAuthorization("root");;
-        // app.MapGet("/rose/{offset}/{limit}", (IRepository<RoseEntity> repository, int offset, int limit) => repository.Get(offset, limit)).RequireAuthorization("root");
-        
         app.MapPut("/EntityExample", (IRepository<EntityExample> repository, EntityExample entity) => repository.InsertAsync(entity));
         app.MapPost("/EntityExample", (IRepository<EntityExample> repository, EntityExample entity) => repository.UpdateAsync(entity));
-        app.MapDelete("/EntityExample", (IRepository<EntityExample> repository, [FromBody]EntityExample entity) => repository.DeleteAsync(entity));
+        app.MapDelete("/EntityExample", (IRepository<EntityExample> repository, [FromBody] EntityExample entity) => repository.DeleteAsync(entity));
         app.MapGet("/EntityExample", (IRepository<EntityExample> repository) => repository.GetListAsync());
         app.MapGet("/EntityExample/IndexInfo", (IRepository<EntityExample> repository) => repository.GetIndexInfo());
         app.MapGet("/EntityExample/{id}/history", (IRepository<EntityExample> repository, string id) => repository.GetHistoryAsync(id));
         app.MapGet("/EntityExample/{id}", (IRepository<EntityExample> repository, string id) => repository.FindByIdAsync(id));
-        app.MapPost("/EntityExample/Paged", (IRepository<EntityExample> repository, [FromBody]DataSourceRequest request) => repository.GetPagedListAsync(request));
-        
+        app.MapPost("/EntityExample/Paged", (IRepository<EntityExample> repository, [FromBody] DataSourceRequest request) => repository.GetPagedListAsync(request));
+
         app.MapPost("/EntityExample/Custom",
             (IRepository<EntityExample> repository, [FromBody] DataSourceRequest request) =>
             {
@@ -36,5 +36,9 @@ public static class RegisterEntities
                 return res.ToList();
             });
 
+        app.MapPost("/BloomFilter/Add", (IBloomFilter bloomFilter, [FromBody] BloomFilterArguments arguments) => bloomFilter.AddItem(arguments.Key, arguments.Item));
+        app.MapPost("/BloomFilter/Cardinality", (IBloomFilter bloomFilter, [FromBody] BloomFilterArguments arguments) => bloomFilter.Cardinality(arguments.Key));
+        app.MapPost("/BloomFilter/Exists", (IBloomFilter bloomFilter, [FromBody] BloomFilterArguments arguments) => bloomFilter.Exists(arguments.Key, arguments.Items));
+        app.MapPost("/BloomFilter/Info", (IBloomFilter bloomFilter, [FromBody] BloomFilterArguments arguments) => bloomFilter.Info(arguments.Key));
     }
 }
---
+++ XProbabilisticToolkit/BloomFilter.cs
@@ -0,0 +1,108 @@
+﻿using StackExchange.Redis;
+
+namespace XProbabilisticToolkit;
+
+public class BloomFilter : IBloomFilter
+{
+    private readonly IDatabase _database;
+
+    public BloomFilter(IDatabase database)
+    {
+        _database = database;
+    }
+
+    public int AddItem(string key, string item)
+    {
+        return (int)_database.Execute("BF.ADD", new List<object>() { key, item });
+    }
+
+    public IDictionary<string, int> AddItems(string key, ISet<string> items)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Cardinality(string key)
+    {
+        return (int)_database.Execute("BF.CARD", new List<object>() { key });
+    }
+
+    public int Exists(string key, string item)
+    {
+        return (int)_database.Execute("BF.EXISTS", new List<object>() { key, item });
+    }
+
+    public IDictionary<string, int> Exists(string key, ISet<string> items)
+    {
+        Dictionary<string, int> values = new Dictionary<string, int>();
+        List<object> arguments = new List<object>() { key };
+        arguments.AddRange(items);
+        RedisResult result = _database.Execute("BF.MEXISTS", arguments);
+        if (result.Type != ResultType.MultiBulk) return values;
+        var resultSet = (RedisResult[])result;
+        arguments.RemoveAt(0);
+        for (int i = 0; i < arguments.Count; i++)
+        {
+            values.Add((string)arguments[i], (int)resultSet[i]);
+        }
+
+        return values;
+    }
+
+    public IDictionary<string, string> Info(string key)
+    {
+        RedisResult commandResult = _database.Execute("BF.INFO", new List<object>() { key });
+        var result = new Dictionary<string, string>();
+        if (commandResult.Type == ResultType.MultiBulk)
+        {
+            var infoPairs = (RedisResult[])commandResult;
+            for (int i = 0; i < infoPairs.Length; i += 2)
+            {
+                string attrName = (string)infoPairs[i];
+                RedisResult attrValue  = infoPairs[i + 1];
+                result.Add(attrName, (string)attrValue );
+            }
+        }
+        return result;
+    }
+
+    public IDictionary<string, int> Insert(BloomFilterArguments bloomFilterArguments)
+    {
+        List<object> arguments = new List<object>();
+        arguments.Add(bloomFilterArguments.Key);
+        if (bloomFilterArguments.Capacity.HasValue)
+        {
+            arguments.Add("CAPACITY");
+            arguments.Add(bloomFilterArguments.Capacity);
+        }
+        
+        if (bloomFilterArguments.Error.HasValue)
+        {
+            arguments.Add("ERROR");
+            arguments.Add(bloomFilterArguments.Error);
+        }
+        
+        if (bloomFilterArguments.Expansion.HasValue)
+        {
+            arguments.Add("EXPANSION");
+            arguments.Add(bloomFilterArguments.Expansion);
+        }
+        if (bloomFilterArguments.NoCreate)
+        {
+            arguments.Add("NOCREATE");
+        }
+        if (bloomFilterArguments.NonScaling)
+        {
+            arguments.Add("NONSCALING");
+        }
+        
+        if (bloomFilterArguments.Items != null)
+        {
+            arguments.Add("ITEMS");
+            arguments.AddRange(bloomFilterArguments.Items);
+        }
+
+        var result = _database.Execute("BF.INSERT", arguments);
+
+        return null;
+    }
+}
