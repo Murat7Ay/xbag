8bc8412f300903787560051eb1977429f3570d5f
Murat Ay <murataybtp@gmail.com>
2024-02-29 01:15:08 +0300

    added simplified hyperloglog

---
+++ XProbabilisticToolkit/ISimpleHyperLogLog.cs
@@ -0,0 +1,21 @@
+﻿namespace XProbabilisticToolkit;
+
+public interface ISimpleHyperLogLog
+{
+    /// <summary>
+    /// Adds elements to the HyperLogLog data structure.
+    /// </summary>
+    /// <param name="elements">The collection of elements to add.</param>
+    void Add(IEnumerable<string> elements);
+
+    /// <summary>
+    /// Deletes the HyperLogLog key from the Redis database.
+    /// </summary>
+    void Delete();
+
+    /// <summary>
+    /// Estimates the cardinality (count of unique elements) added to the HyperLogLog data structure.
+    /// </summary>
+    /// <returns>An estimate of the cardinality.</returns>
+    long EstimateCardinality();
+}
---
+++ XProbabilisticToolkit/SimpleHyperLogLog.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+namespace XProbabilisticToolkit;
+
+public class SimpleHyperLogLog : ISimpleHyperLogLog
+{
+    private readonly string _hyperLogLogKey;
+    private readonly IDatabase _database;
+
+    public SimpleHyperLogLog(String hyperLogLogKey, IDatabase database)
+    {
+        _hyperLogLogKey = hyperLogLogKey;
+        _database = database;
+    }
+
+    public void Add(IEnumerable<string> elements)
+    {
+        var parameters = new List<object> { _hyperLogLogKey };
+        parameters.AddRange(elements);
+        _database.Execute("PFADD", parameters);
+    }
+
+    public void Delete()
+    {
+        _database.Execute("DEL", new List<object>() { _hyperLogLogKey });
+    }
+
+    public long EstimateCardinality()
+    {
+        return (long)_database.Execute("PFCOUNT", new List<object>() { _hyperLogLogKey });
+    }
+}
