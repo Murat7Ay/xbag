0d76a56214c22d946a2ddc1db3f426cc16002310
Murat Ay <murataybtp@gmail.com>
2024-08-04 00:06:15 +0300

    added properties to authuser

--- XDataAccess/AuthUser.cs
+++ XDataAccess/AuthUser.cs
@@ -6,4 +6,6 @@ public  class AuthUser : IAuthUser
     public string? Ip { get; set; } = "TEST_IP";
     public string? Host { get; set; } = "TEST_HOST";
     public string? TraceId { get; set; } = "TEST_TRACEID";
+    public string? Name { get; set; } = "Murat";
+    public string? Surname { get; set; } = "Ay";
 }
